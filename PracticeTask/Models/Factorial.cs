5d5c0122dfc6988def10693048ebcc5166ee62e2
Anar Gandayev <tu7bxfga8@aztu.local>
2022-04-06 17:02:38 +0400

    commit

---
+++ PracticeTask/Models/Factorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeTask.Models
+{
+    class Factorial
+    {
+        public int number { get; set; }
+        public int FactorialCalculate(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+
+            }
+            else if (number<0)
+            {
+                return -1;
+            }
+            else
+            {
+              return number * FactorialCalculate(number - 1);
+                    
+            }
+        }
+    }
+}
---
+++ PracticeTask/Program.cs
@@ -0,0 +1,19 @@
+﻿using PracticeTask.Models;
+using System;
+
+namespace PracticeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Factorial fc = new Factorial();
+            Console.WriteLine("Eded daxil edin");
+            fc.number=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Factorial=" + fc.FactorialCalculate(fc.number));
+          
+            
+        }
+        
+    }
+}
